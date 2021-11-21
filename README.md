ab504bc9acfdde39559dc943b74ab123dde49b91
Abdullah <abdullah_a_s@hotmail.co.uk>
2021-11-21 21:04:03 +0000

    Âremoved unused commented code and updated the readme file

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# game-challenge
+# Game-challenge: Simon
+## Tech Stack
+ - HTML
+ - CSS
+ - SCSS
+ - JavaScript
+
+## The Design
+The design was created using a mobile-first approach with the goal of having a responsive layout. A flex container was created with four child div elements which were then styled into the shape of coloured pads using border-radius. A combination of h and p elements were used for the text, and a button element was used to start the game.
+
+## The Code
+A click event was added to the start button which calls a function that clears the randomPads and userPads arrays, removes the "GAME OVER" text if displayed and then calls the playGame function.
+
+The playGame function randomly pushes one of the pads to the randomPads array and then performs a forEach on the same array. The forEach calls the comparePads funtion on each element in the array with a setTimeout.
+
+The comparePads function uses an if statement to determine the selected pad in the randomPads array and then applies a temporary colour change and box-shadow using a setTimeout to simulate a light flash.
+
+Following this order of events the user clicks on the pads in an attempt to copy the random sequence. Using click events, when the user clicks on a pad a similar function is performed to that of comparePads where a "light flash" occurs and the selected pad is pushed into the userPads array.
+
+The compareArrays function uses an if statement to determine if the randomPads and userPads arrays are equal. If so, the score is increased by one and the original playGame function is called repeating the whole process. If the arrays are not equal (the user selected an incorrect sequence) a "GAME OVER" text is displayed and the sequence breaks.
--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-//Need to link pads and button from the html by selecting them via the queryselector
-
 let start = document.querySelector(".start-btn");
 let score = document.querySelector(".score");
 let gameOver = document.querySelector("h3");
@@ -9,18 +7,81 @@ let pad2 = document.querySelector("#board__pad2");
 let pad3 = document.querySelector("#board__pad3");
 let pad4 = document.querySelector("#board__pad4");
 
+//Create a variable used to increase the score
+let increaseScore = 0;
 
+//Create empty arrays one for the randomly selected sequence and one for the user choices
+let randomPads = [];
+let userPads = [];
 
-//Click the start button needs to reset the score to zero and start the game
+//create a click event for the start-btn to start the game 
+const startGame = start.addEventListener("click", () => {
+    //the "GAME OVER" sign is removed, the score is reset to zero, the randomPads and userPads arrays are cleared, and the playGame function is called
+    gameOver.style.display = "none";
+    score.innerHTML = `SCORE = ${increaseScore = 0}`;
+    randomPads = [];
+    userPads = [];
+    setTimeout(() => {
+        playGame();
+    }, 500);
+}); 
 
-//Need a for loop to iterate through an array of the pads while randomly selecting one and triggering a light/color change, at the same time pushing the selected pad to an empty array.  
-//User clicks pads trying to imitate the sequence, each pad clicked will be sent to another empty array which will be compared to the array of random pads in an "if" statement. If the 
-//two arrays are equal the for loop repeats the iteration adding another random pad to the sequence. If the arrays are not equal the loop ends and game is over
+//create a playGame function which selects a random pad and pushes in into the randomPads array
+const playGame = () => {
+    const random = pads[Math.floor(Math.random() * pads.length)];
+    randomPads.push(random);
+    console.log(randomPads)
+    //Perform a forEach on the randomPads array 
+    randomPads.forEach((pad, i) => {
+    //Call the comparePads function on each element and apply a setTimeout to have a time delay between each color flash 
+        setTimeout(() => {
+            comparePads(pad); 
+        }, i * 700);               
+    });
+}
 
-//Create an array containing the pads
-// const boardPads = [pads];
+//create a comparePads function using an if statment to determine which pad is selected in the array and to the display the correct 'color flash' (a change in the background color and 
+//box-shadow) and then apply a setTimeout to revert back to the original colour in a specified time
+const comparePads = (pad) => {
+    if (pad === pad1) {
+    pad1.style.backgroundColor = "#32df32";
+    pad1.style.boxShadow = "-2px 5px 50px 10px #a7fba7";
+    setTimeout(padTimeOut1, 300)
+    } else if (pad === pad2) {
+    pad2.style.backgroundColor = "#e93f3f";
+    pad2.style.boxShadow = "2px 5px 50px 10px lightcoral";
+    setTimeout(padTimeOut2, 300)
+    } else if (pad === pad3) {
+    pad3.style.backgroundColor = "#fcff52";
+    pad3.style.boxShadow = "-2px -5px 50px 10px #c0c168";
+    setTimeout(padTimeOut3, 300)
+    } else if (pad === pad4) {
+    pad4.style.backgroundColor = "#006bff";
+    pad4.style.boxShadow = "2px -5px 50px 10px #687ec1";
+    setTimeout(padTimeOut4, 300)
+    }
+}
 
-//Create click events for each pad and push the selection into userPads
+//Create functions to setTimeout to revert the pad color after the 'colour flash'
+const padTimeOut1 = () => {
+    pad1.style.backgroundColor = "#61D48F";
+    pad1.style.boxShadow = "";
+}; 
+const padTimeOut2 = () => {
+    pad2.style.backgroundColor = "#D85E5B";
+    pad2.style.boxShadow = "";
+}; 
+const padTimeOut3 = () => {
+    pad3.style.backgroundColor = "#EBEC77";
+    pad3.style.boxShadow = "";
+}; 
+const padTimeOut4 = () => {
+    pad4.style.backgroundColor = "#32A4EA";
+    pad4.style.boxShadow = "";
+}; 
+
+//Create click events for each pad, and when clicked trigger a change in background colour and box-shadow colour which is then reverted using a setTimeout. The selected pad is then 
+//pushed into userPads and the compareArrays function is called
 const userClick1 = pad1.addEventListener("click", () => {
     const padFlash1 = pad1.style.backgroundColor = "#32df32";
     const borderFlash1 = pad1.style.boxShadow = "-2px 5px 50px 10px #a7fba7";
@@ -50,109 +111,22 @@ const userClick4 = pad4.addEventListener("click", () => {
     compareArrays();
 })
 
-//Create empty arrays one for the randomly selected sequence and one for the user choices
-let randomPads = [];
-let userPads = [];
-
-//Create variables to setTimeout to revert the pad color after "flash"
-const padTimeOut1 = () => {
-    pad1.style.backgroundColor = "#61D48F";
-    pad1.style.boxShadow = "";
-}; 
-const padTimeOut2 = () => {
-    pad2.style.backgroundColor = "#D85E5B";
-    pad2.style.boxShadow = "";
-}; 
-const padTimeOut3 = () => {
-    pad3.style.backgroundColor = "#EBEC77";
-    pad3.style.boxShadow = "";
-}; 
-const padTimeOut4 = () => {
-    pad4.style.backgroundColor = "#32A4EA";
-    pad4.style.boxShadow = "";
-}; 
-
-//create function for the if statment to determine correct color flash
-const comparePads = (pad) => {
-    if (pad === pad1) {
-    pad1.style.backgroundColor = "#32df32";
-    pad1.style.boxShadow = "-2px 5px 50px 10px #a7fba7";
-    setTimeout(padTimeOut1, 300)
-    } else if (pad === pad2) {
-    pad2.style.backgroundColor = "#e93f3f";
-    pad2.style.boxShadow = "2px 5px 50px 10px lightcoral";
-    setTimeout(padTimeOut2, 300)
-    } else if (pad === pad3) {
-    pad3.style.backgroundColor = "#fcff52";
-    pad3.style.boxShadow = "-2px -5px 50px 10px #c0c168";
-    setTimeout(padTimeOut3, 300)
-    } else if (pad === pad4) {
-    pad4.style.backgroundColor = "#006bff";
-    pad4.style.boxShadow = "2px -5px 50px 10px #687ec1";
-    setTimeout(padTimeOut4, 300)
-    }
-}
-
-//create a click event for the start-btn to start the game 
-// const startGame = start.addEventListener("click", () => {
-
-//     for (let i =0; i <= 20; i++) {
-//         //Create a variable that picks a random pad from the array and pushes it to the randomPads array
-//         const random = pads[Math.floor(Math.random() * pads.length)];
-//         randomPads.push(random);
-//         console.log(randomPads)
-//         //Use forEach on the randomPads array to apply color flash to each item 
-//         randomPads.forEach((pad, i) => {
-        
-//             //Call the comparePads function and apply a setTimeout to have a time delay between each color flash 
-//             setTimeout(() => {
-//                 comparePads(pad); 
-//             }, i * 1000);
-
-            
-//         });
-
-//         //Create if statement to deterimine if randomPads and userPads are equal in length
-//         if (randomPads.length === userPads.length) {
-//             if (randomPads !== userPads) {
-//                 break
-//             } else {
-//             }
-//         }
-//     }
-    
-// })
-
-let increaseScore = 0;
-
-const playGame = () => {
-    const random = pads[Math.floor(Math.random() * pads.length)];
-    randomPads.push(random);
-    console.log(randomPads)
-    randomPads.forEach((pad, i) => {
-        
-    //Call the comparePads function and apply a setTimeout to have a time delay between each color flash 
-    setTimeout(() => {
-        comparePads(pad); 
-    }, i * 700);
-        
-                    
-    });
-}
-
 //create a function that compares the arrays via an if statement and for loop
 const compareArrays = () => { 
     console.log(randomPads, userPads);
     if (randomPads.length !== userPads.length) {
+        //if the arrays are not equal in length i.e. the user has not seleceted the sequence yet then the for loop does not trigger
         return
     } else {
         for (let i = 0; i < randomPads.length; i++) {
             console.log(randomPads[i], userPads[i]);
             if (randomPads[i] !== userPads[i]) {
+                //if the arrays are not equal i.e. the user has selected an incorrect sequence then display "GAME OVER" and end the loop
                 gameOver.style.display = "block";
                 return;
             }
         }
+    //if the arrays are equal then the playGame function is called, the userPads array is cleared and the score is increased by one
     setTimeout(() => {
         playGame();
     }, 1000);
@@ -161,16 +135,3 @@ const compareArrays = () => {
     }  
 }  
 
-
-const startGame = start.addEventListener("click", () => {
-    gameOver.style.display = "none";
-    score.innerHTML = `SCORE = ${increaseScore = 0}`;
-    randomPads = [];
-    userPads = [];
-    setTimeout(() => {
-        playGame();
-    }, 500);
-});    
-//dont need a for loop on the "pads" that can be a simple random number generator which sends a new pad to the randomPads array, when the user clicks the correct sequence an
-//if statement send back to the random generator adding a new pad to the sequence. Where the for loops come into play is when you want to display the sequence, so the for loop will be 
-//for the randomPads
